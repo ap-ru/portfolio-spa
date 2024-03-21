9ab0094287cacbc58539685b1cb6f9fce570f52d
Anish <anishp2303@gmail.com>
2024-03-21 19:05:57 -0400

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>About me and my skills.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h2>Portfolio Page</h2>
+      <p>My projects and works.</p>
+    </div>
+  );
+};
+
+export default Portfolio;
